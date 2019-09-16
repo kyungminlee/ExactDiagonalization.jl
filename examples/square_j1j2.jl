9b3880927bea3978d104bae1be4f2715c3a6584c
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-09-16 12:15:05 -0400

    Make square_j1j2.jl use Memento

--- examples/square_j1j2.jl
+++ examples/square_j1j2.jl
@@ -2,8 +2,9 @@ using LinearAlgebra
 using SparseArrays
 using Arpack
 using JLD2
-
 using ExactDiagonalization
+using ArgParse
+using Memento
 
 function make_square_lattice(n1 ::Integer, n2 ::Integer)
   n_sites = n1 * n2
@@ -68,106 +69,147 @@ function make_square_lattice(n1 ::Integer, n2 ::Integer)
   return (nearest_neighbor_pairs, second_nearest_neighbor_pairs, chiral_triplets)
 end
 
+function make_J1J2J3_hamiltonian(hs::AbstractHilbertSpace, n1::Integer, n2::Integer)
+  
+  PAULI_MATRICES = [ Float64[0 1.0; 1.0 0.0],
+                     ComplexF64[0.0 -1.0*im; 1.0*im 0.0],
+                     Float64[1.0 0.0; 0.0 -1.0] ]
+
+  function σ(i ::Integer, j::Symbol) ::KroneckerProductOperator{ComplexF64}
+    if j == :x
+      return KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>PAULI_MATRICES[1]))
+    elseif j == :y
+      return KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>PAULI_MATRICES[2]))
+    elseif j == :z
+      return KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>PAULI_MATRICES[3]))
+    elseif j == :+
+      return KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>[0.0 1.0; 0.0 0.0]))
+    elseif j == :-
+      return KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>[0.0 0.0; 1.0 0.0]))
+    else
+      throw(ArgumentError("pauli matrix of type $(j) not supported"))
+    end
+  end
+
+  (nearest_neighbor_pairs, second_nearest_neighbor_pairs, chiral_triplets) = make_square_lattice(n1, n2)
+
+  j1_terms = KroneckerProductOperator{ComplexF64}[]
+  for (i,j) in nearest_neighbor_pairs
+    push!(j1_terms, 2 * σ(i, :+) * σ(j, :-))
+    push!(j1_terms, 2 * σ(i, :-) * σ(j, :+))
+    push!(j1_terms, σ(i, :z) * σ(j, :z))
+  end
+
+  j2_terms = KroneckerProductOperator{ComplexF64}[]
+  for (i,j) in second_nearest_neighbor_pairs
+    push!(j2_terms, 2 * σ(i, :+) * σ(j, :-))
+    push!(j2_terms, 2 * σ(i, :-) * σ(j, :+))
+    push!(j2_terms, σ(i, :z) * σ(j, :z))
+  end
+
+  j3_terms = KroneckerProductOperator{ComplexF64}[]
+  for (i,j,k) in chiral_triplets
+    push!(j3_terms, 2*im * σ(i, :z) * σ(j, :+) * σ(k, :-))
+    push!(j3_terms,-2*im * σ(i, :z) * σ(j, :-) * σ(k, :+))
+
+    push!(j3_terms, 2*im * σ(j, :z) * σ(k, :+) * σ(i, :-))
+    push!(j3_terms,-2*im * σ(j, :z) * σ(k, :-) * σ(i, :+))
+    
+    push!(j3_terms, 2*im * σ(k, :z) * σ(i, :+) * σ(j, :-))
+    push!(j3_terms,-2*im * σ(k, :z) * σ(i, :-) * σ(j, :+))
+  end
+  return (j1_terms, j2_terms, j3_terms)
+end
+
+
+
+
+function parse_commandline()
+  s = ArgParseSettings()
+  @add_arg_table s begin
+    "--n1"
+      arg_type = Int
+      required = true
+    "--n2"
+      arg_type = Int
+      required = true
+    "--sz"
+      arg_type = Int
+      required = true
+  end
+  return parse_args(s)
+end
+
+
 function main()
+  main_logger = Memento.config!("info"; fmt="[{date} | {level} | {name}]: {msg}")
+
+  args = parse_commandline()
+
   QN = Int
   up = State{QN}("Up", 1)
   dn = State{QN}("Dn",-1)
   spinsite = Site{QN}([up, dn])
 
-  (n1, n2) = (4, 4)
-  n_sites = n1 * n2
+  #(n1, n2) = (4, 4)
+  n1 = args["n1"]
+  n2 = args["n2"]
+  qn = args["sz"]
   
-  hs = AbstractHilbertSpace{QN}()
-  for i in 1:n_sites
-    add_site!(hs, spinsite)
-  end
+  n_sites = n1 * n2
+  info(main_logger, "Parameteters : n1 = $n1, n2 = $n2, sz = $qn")
+  
+  info(main_logger, "Building AbstractHilbertSpace")
+  hs = AbstractHilbertSpace([spinsite for i in 1:n_sites])
+  #for i in 1:n_sites
+  #  add_site!(hs, spinsite)
+  #end
 
-  @show hs
+  info(main_logger, "Making J1, J2, J3 terms")
+  (j1_terms, j2_terms, j3_terms) = make_J1J2J3_hamiltonian(hs, n1, n2)
 
-  PAULI_MATRICES = [ Float64[0 1.0; 1.0 0.0], ComplexF64[0.0 -1.0*im; 1.0*im 0.0], Float64[1.0 0.0; 0.0 -1.0]]
+  #sectors = quantum_number_sectors(hs)
+  #sectors = [x for x in sectors if x >= 0]
+  #for qn in sectors
+  info(main_logger, "Sector = $qn")
+  info(main_logger, "Concretizing Hilbert Space"); flush(stdout)
+  chs = concretize(hs, Set([qn]))
+  info(main_logger, "Hilbert space dimension = $(length(chs.basis_list))")
 
-  sigma(i ::Integer, j ::Integer) = KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>PAULI_MATRICES[j]))
-  sigma_plus(i ::Integer) = KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>[0.0 1.0; 0.0 0.0]))
-  sigma_minus(i ::Integer) = KroneckerProductOperator{ComplexF64}(hs, 1.0, Dict(i=>[0.0 0.0; 1.0 0.0]))
+  info(main_logger, "Materializing J1"); flush(stdout)
+  j1_sparse, ϵ = materialize_parallel(chs, j1_terms)
+  info(main_logger, "Number of nonzero values = $(length(j1_sparse.nzval))")
+  info(main_logger, "Bound for error = $ϵ")
 
-  (nearest_neighbor_pairs, second_nearest_neighbor_pairs, chiral_triplets) = make_square_lattice(n1, n2)
+  info(main_logger, "Materializing J2"); flush(stdout)
+  j2_sparse, ϵ = materialize_parallel(chs, j2_terms)
+  info(main_logger, "Number of nonzero values = $(length(j2_sparse.nzval))")
+  info(main_logger, "Bound for error = $ϵ")
 
-  (J1, J2, J3) = (1.0, 1.0, 1.0)
+  info(main_logger, "Materializing J3"); flush(stdout)
+  j3_sparse, ϵ = materialize_parallel(chs, j3_terms)
+  info(main_logger, "Number of nonzero values = $(length(j3_sparse.nzval))")
+  info(main_logger, "Bound for error = $ϵ")
 
-  J2s = 0:0.2:2
-  J3s = 0:0.2:2
+  spectrum = Dict()
+  for J2 in J2s, J3 in J3s
+    info(main_logger, "Starting (J1, J2, J3) = ($J1, $J2, $J3)")
+    info(main_logger, "Constructing the total Hamiltonian")
+    H = J1 * j1_sparse + J2 * j2_sparse + J3 * j3_sparse
 
-  j1_terms = KroneckerProductOperator{ComplexF64}[]
-  for (i,j) in nearest_neighbor_pairs
-    push!(j1_terms, 2 * sigma_plus(i) * sigma_minus(j))
-    push!(j1_terms, 2 * sigma_minus(i) * sigma_plus(j))
-    push!(j1_terms, sigma(i,3) * sigma(j,3))
-  end
-
-  j2_terms = KroneckerProductOperator{ComplexF64}[]
-  for (i,j) in second_nearest_neighbor_pairs
-    push!(j2_terms, 2 * sigma_plus(i) * sigma_minus(j))
-    push!(j2_terms, 2 * sigma_minus(i) * sigma_plus(j))
-    push!(j2_terms, sigma(i,3) * sigma(j,3))
-  end
-
-  j3_terms = KroneckerProductOperator{ComplexF64}[]
-  for (i,j,k) in chiral_triplets
-    #push!(j3_terms, sigma(i,1) * sigma(j,2) * sigma(k, 3))
-    #push!(j3_terms, sigma(i,2) * sigma(j,3) * sigma(k, 1))
-    #push!(j3_terms, sigma(i,3) * sigma(j,1) * sigma(k, 2))
-    push!(j3_terms, 2*im * sigma(i,3)* sigma_plus(j) * sigma_minus(k))
-    push!(j3_terms,-2*im * sigma(i,3)* sigma_minus(j) * sigma_plus(k))
-
-    push!(j3_terms, 2*im * sigma(j,3)* sigma_plus(k) * sigma_minus(i))
-    push!(j3_terms,-2*im * sigma(j,3)* sigma_minus(k) * sigma_plus(i))
-    
-    push!(j3_terms, 2*im * sigma(k,3)* sigma_plus(i) * sigma_minus(j))
-    push!(j3_terms,-2*im * sigma(k,3)* sigma_minus(i) * sigma_plus(j))
-    
-  end
-
-
-
-  sectors = quantum_number_sectors(hs)
-  sectors = [x for x in sectors if x >= 0]
-
-  for qn in sectors
-    println("------------------------------")
-    println("Sector = ", qn)
-    println("Concretizing Hilbert Space")
-    flush(stdout)
-    
-    chs = concretize(hs, Set([qn]))
-    println("Materializing Terms")
-    flush(stdout)
-    j1_sparse, ϵ = materialize_parallel(chs, j1_terms)
-    @show "J1", ϵ
-    j2_sparse, ϵ = materialize_parallel(chs, j2_terms)
-    @show "J2", ϵ
-    j3_sparse, ϵ = materialize_parallel(chs, j3_terms)
-    @show "J3", ϵ
-
-    spectrum = Dict()
-    #spectrum ::Dict{Int, Vector{Float64}} = if size(H)[1] <= 20
-    for J2 in J2s, J3 in J3s
-      @show (J1, J2, J3)
-
-      H = J1 * j1_sparse + J2 * j2_sparse + J3 * j3_sparse
-      println("Diagonlizating Hamiltonian")
-      flush(stdout)
-
-      if size(H)[1] <= 20
-        spectrum[(J1, J2, J3)] = sort(eigvals(Hermitian(Matrix(H))))
-      else
-        (eigenvalues, eigenvectors) = eigs(H; which=:SR)
-        spectrum[(J1, J2, J3)] = sort(real.(eigenvalues))
-      end
-
-    end # for J2, J3
-    filename = "spectrum_$(n1)_$(n2)_$(qn).jld2"
-    @save filename n1, n2, qn, spectrum
-  end # for qn
+    if size(H)[1] <= 20
+      info(main_logger, "Diagonalizing dense Hamiltonian (size = $(size(H)[1]))")
+      spectrum[(J1, J2, J3)] = sort(eigvals(Hermitian(Matrix(H))))
+    else
+      info(main_logger, "Diagonalizing sparse Hamiltonian (size = $(size(H)[1]))")
+      (eigenvalues, eigenvectors) = eigs(H; which=:SR)
+      spectrum[(J1, J2, J3)] = sort(real.(eigenvalues))
+    end
+  end # for J2, J3
+  filename = "spectrum_$(n1)_$(n2)_$(qn).jld2"
+  info(main_logger, "Saving $filename")
+  @save filename n1, n2, qn, spectrum
+  #end # for qn
 end
 
 main()
--- src/HilbertSpace/abstract_hilbert_space.jl
+++ src/HilbertSpace/abstract_hilbert_space.jl
@@ -10,12 +10,17 @@ mutable struct AbstractHilbertSpace{QN}
 
   AbstractHilbertSpace{QN}() where {QN} = new{QN}([], [], [0])
   function AbstractHilbertSpace(sites ::AbstractArray{Site{QN}, 1}) where QN
-      bitwidths = UInt[bitwidth(site) for site in sites]
-      bitoffsets = UInt[0, cumsum(bitwidths)...]
-      #bitmasks = UInt[make_bitmask(msb=bitoffset[i] + bitwidths[i]; lsb=bitoffset[i], dtype=UInt) for i in 1:length(sites)]
-
-      new{QN}(sites, bitwidths, bitoffsets) #, bitmasks)
+    bitwidths = UInt[bitwidth(site) for site in sites]
+    bitoffsets = UInt[0, cumsum(bitwidths)...]
+    #bitmasks = UInt[make_bitmask(msb=bitoffset[i] + bitwidths[i]; lsb=bitoffset[i], dtype=UInt) for i in 1:length(sites)]
+    new{QN}(sites, bitwidths, bitoffsets) #, bitmasks)
   end
+
+  function AbstractHilbertSpace{QN}(sites ::AbstractArray{Site{QN}, 1}) where QN
+    bitwidths = UInt[bitwidth(site) for site in sites]
+    bitoffsets = UInt[0, cumsum(bitwidths)...]
+    new{QN}(sites, bitwidths, bitoffsets) #, bitmasks)
+  end  
 end
 
 import Base.==
