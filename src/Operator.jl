91ab089d7eb3926744f4d3d88095bb905f7d2c49
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-09-18 11:23:06 -0400

    Drop KPO

--- src/Operator.jl
+++ src/Operator.jl
@@ -4,7 +4,7 @@ include("Operator/nullop.jl")
 include("Operator/pureop.jl")
 include("Operator/sumop.jl")
 
-include("Operator/kpo.jl")
+#include("Operator/kpo.jl")
 
 include("Operator/operator_application.jl")
 include("Operator/operator_simplify.jl")
