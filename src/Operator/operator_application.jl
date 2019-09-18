5d3bc19c1cc5495eeb9be3ab68f6bb44fe608b7e
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-09-18 11:18:27 -0400

    Comment out kronecker product operator application

--- src/Operator/operator_application.jl
+++ src/Operator/operator_application.jl
@@ -1,70 +1,67 @@
 export apply
 export materialize, materialize_parallel
 
-"""
-Returns dict
-"""
-function apply(hs ::AbstractHilbertSpace,
-               op ::KroneckerProductOperator{OS},
-               binrep ::BR) where {OS<:Number, BR<:Unsigned}
-  zero_scalar = zero(OS)
-  output = Dict{BR, OS}(binrep => 1.0)
-
-  for (isite, siteop) in op.operators
-    @assert 1 <= isite <= length(hs.sites)
-    nextoutput = DefaultDict{BR, OS}(zero_scalar)
-    sitedim = length(hs.sites[isite].states)
-    for (r ::BR, amplitude ::OS) in output
-      sri = get_state_index(hs, r, isite) # site row index
-      for sci in 1:sitedim # site col index
-        value = siteop[sri, sci]
-        if ! isapprox(value, 0)
-          c = update(hs, r, isite, sci)
-          nextoutput[c] += value * amplitude
-        end
-      end
-    end
-    output = nextoutput
-  end
-
-  for (k, v) in output
-    output[k] = v * op.amplitude
-  end
-
-  return output
-end
+# """
+# Returns dict
+# """
+# function apply(hs ::AbstractHilbertSpace,
+#                op ::KroneckerProductOperator{OS},
+#                binrep ::BR) where {OS<:Number, BR<:Unsigned}
+#   zero_scalar = zero(OS)
+#   output = Dict{BR, OS}(binrep => 1.0)
+#   for (isite, siteop) in op.operators
+#     @assert 1 <= isite <= length(hs.sites)
+#     nextoutput = DefaultDict{BR, OS}(zero_scalar)
+#     sitedim = length(hs.sites[isite].states)
+#     for (r ::BR, amplitude ::OS) in output
+#       sri = get_state_index(hs, r, isite) # site row index
+#       for sci in 1:sitedim # site col index
+#         value = siteop[sri, sci]
+#         if ! isapprox(value, 0)
+#           c = update(hs, r, isite, sci)
+#           nextoutput[c] += value * amplitude
+#         end
+#       end
+#     end
+#     output = nextoutput
+#   end
+#   for (k, v) in output
+#     output[k] = v * op.amplitude
+#   end
+#   return output
+# end
 
 
-function apply(hs ::AbstractHilbertSpace,
-               ops ::AbstractArray{KroneckerProductOperator{OS}},
-               binrep ::BR) where {OS<:Number, BR <:Unsigned}
-  zero_scalar = zero(OS)
-  results = DefaultDict{BR, OS}(zero_scalar)
-  for op in ops
-    output = apply(hs, op, binrep)
-    for (row, amplitude) in output
-      results[row] += amplitude
-    end
-  end
-  return results
-end
+# function apply(hs ::AbstractHilbertSpace,
+#                ops ::AbstractArray{KroneckerProductOperator{OS}},
+#                binrep ::BR) where {OS<:Number, BR <:Unsigned}
+#   zero_scalar = zero(OS)
+#   results = DefaultDict{BR, OS}(zero_scalar)
+#   for op in ops
+#     output = apply(hs, op, binrep)
+#     for (row, amplitude) in output
+#       results[row] += amplitude
+#     end
+#   end
+#   return results
+# end
 
 
-function apply(hs ::AbstractHilbertSpace,
-               ops ::AbstractArray{KroneckerProductOperator{OS}},
-               psi ::AbstractDict{BR, SS}) where {OS<:Number, SS<:Number, BR<:Unsigned}
-  OutScalar = promote_type(OS, SS)
-  zero_scalar = zero(OutScalar)
-  results = DefaultDict{BR, OutScalar}(zero_scalar)
+# function apply(hs ::AbstractHilbertSpace,
+#                ops ::AbstractArray{KroneckerProductOperator{OS}},
+#                psi ::AbstractDict{BR, SS}) where {OS<:Number, SS<:Number, BR<:Unsigned}
+#   OutScalar = promote_type(OS, SS)
+#   zero_scalar = zero(OutScalar)
+#   results = DefaultDict{BR, OutScalar}(zero_scalar)
 
-  for (binrep, amplitude) in psi
-    output = apply(hs, ops, binrep)
-    for (ϕ, α) in output
-      results[binrep] += amplitude * α 
-    end
-  end
-  return results
-end
+#   for (binrep, amplitude) in psi
+#     output = apply(hs, ops, binrep)
+#     for (ϕ, α) in output
+#       results[binrep] += amplitude * α 
+#     end
+#   end
+#   return results
+# end
 
 import Base.isempty
 isempty(psi::SparseState{S2, BR}) where {S2, BR} = isempty(psi.components)
