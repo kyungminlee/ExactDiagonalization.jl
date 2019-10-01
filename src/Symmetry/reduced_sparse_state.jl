be5d80eb6d087f567b7e69b64f5beecfc7c1fe9c
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-01 14:36:40 +0900

    Add reduced_sparse_state (WIP)

--- src/Symmetry/reduced_sparse_state.jl
+++ src/Symmetry/reduced_sparse_state.jl
@@ -1,6 +1,6 @@
 export ReducedSparseState
-export get_component
-export set_component!
+# export get_component
+# export set_component!
 
 struct ReducedSparseState
   reduced_hilbert_space_realization ::ReducedHilbertSpaceRealization
@@ -10,7 +10,7 @@ struct ReducedSparseState
   end
 end
 
-"""
+#=
 |R(001)> = bᵢ(001) |001> + bᵢ(010) |010> + bᵢ(100) |100> + ...
 (symmetry related)
 
@@ -18,8 +18,9 @@ end
 
 # Return
   (a(R(bvec)) * b_i(bvec)
-"""
-function get_component(rss ::ReducedSparseState, bvec::UInt) ::ComplexF64
+=#
+import Base.getindex
+function getindex(rss ::ReducedSparseState, bvec::UInt) ::ComplexF64
   lookup = rss.reduced_hilbert_space_realization.basis_lookup
   ivec, amplitude = lookup[bvec]
   #(_, amplitude_parent) = lookup[ivec]
@@ -27,13 +28,14 @@ function get_component(rss ::ReducedSparseState, bvec::UInt) ::ComplexF64
 end
 
 
-"""
+#=
 |R(001)> = bᵢ(001) |001> + bᵢ(010) |010> + bᵢ(100) |100> + ...
 |ψᵣ> = a(001) |R(001)> + a(011) |R(011)> + ...
 
 Set a(R(bvec)) = value / b(bvec)
-"""
-function set_component!(rss ::ReducedSparseState, value ::ComplexF64, bvec ::UInt)
+=#
+import Base.setindex!
+function setindex!(rss ::ReducedSparseState, value ::ComplexF64, bvec ::UInt)
   list = rss.reduced_hilbert_space_realization.basis_list
   lookup = rss.reduced_hilbert_space_realization.basis_lookup
   ivec, amplitude = lookup[bvec]
