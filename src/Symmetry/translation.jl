dd010aa26f2eb5c05ffe131b2bb4725365f73d29
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-02 21:16:05 +0900

    Add message to assertion

--- src/Symmetry/translation.jl
+++ src/Symmetry/translation.jl
@@ -12,12 +12,14 @@ struct TranslationGroup <: AbstractSymmetryGroup
   character_table ::Array{ComplexF64, 2}
 
   function TranslationGroup(generators::AbstractArray{Permutation})
-    @assert all(g1 * g2 == g2 * g1 for g1 in generators, g2 in generators)
+    @assert all(g1 * g2 == g2 * g1 for g1 in generators, g2 in generators) "non-commuting set of generators"
 
     shape = [g.cycle_length for g in generators]
     translations = vcat( collect( Iterators.product([0:g.cycle_length-1 for g in generators]...) )...)
     translations = [ [x...] for x in translations]
     elements = [prod(gen^d for (gen, d) in zip(generators, dist)) for (ig, dist) in enumerate(translations)]
+
+    @assert length(Set(elements)) == length(elements) "elements not unique (generators not orthogonal)"
 
     momentum(sub) = [x//d for (x, d) in zip(sub, shape)]
     fractional_momenta = [momentum(sub) for sub in translations]
