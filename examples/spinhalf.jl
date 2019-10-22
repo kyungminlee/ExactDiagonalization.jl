e45bdfd5a95c362a976cd6129942050625398356
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-22 01:43:58 -0400

    Update examples/spinhalf.jl

--- examples/spinhalf.jl
+++ examples/spinhalf.jl
@@ -1,13 +1,13 @@
 using ExactDiagonalization
 
-QN = Int
-up = State{QN}("Up", 1)
-dn = State{QN}("Dn",-1)
-spin_site = Site{QN}([up, dn])
+QN = Int;
+up = State{QN}("Up", 1);
+dn = State{QN}("Dn",-1);
+spin_site = Site{QN}([up, dn]);
 
-n_sites = 4
-hs = HilbertSpace([spin_site for i in 1:n_sites])
-hsr = realize(hs, QN(0))
+n_sites = 4;
+hs = HilbertSpace([spin_site for i in 1:n_sites]);
+hsr = realize(hs, QN(0));
 
 function pauli_matrix(hs::HilbertSpace, isite ::Integer, j ::Symbol)
   if j == :x
@@ -23,8 +23,24 @@ function pauli_matrix(hs::HilbertSpace, isite ::Integer, j ::Symbol)
   else
     throw(ArgumentError("pauli matrix of type $(j) not supported"))
   end
+end;
+
+σ = Dict( (isite, j) => pauli_matrix(hs, isite, j) for isite in 1:n_sites, j in [:x, :y, :z, :+, :-]);
+
+j1 = sum(σ[(i, j)] * σ[( mod(i, n_sites) + 1 , j)] for i in 1:n_sites, j in [:x, :y, :z]);
+
+j1_r = represent(hsr, j1)
+
+n = dimension(hsr)
+for icol in 1:n
+  bcol = hsr.basis_list[icol]
+  @show bcol
+  for (k, v) in collect(get_slice(j1, bcol))
+    println(get(hsr.basis_lookup, k, -1) => v)
+  end
+  # ket = zeros(ComplexF64, n)
+  # bra = zeros(ComplexF64, n)
+  # ket[irow] = 1.0
+  # apply_unsafe!(bra, j1_r, ket)
+  # println(real.(bra))
 end
-
-σ = Dict( (isite, j) => pauli_matrix(hs, isite, j) for isite in 1:n_sites, j in [:x, :y, :z, :+, :-])
-
-j1 = sum(σ[(i, j)] * σ[( mod(i, n_sites) + 1 , j)] for i in 1:n_sites, j in [:x, :y, :z])
