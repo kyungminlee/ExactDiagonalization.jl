7163f32bc930b3e96e45a7f8a8679e07dc82f28f
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-21 23:38:41 -0400

    Dense state ket

--- src/StateKet/sparse_state_indexed.jl
+++ src/StateKet/sparse_state_indexed.jl
@@ -1,217 +1,225 @@
+export SparseStateIndexed
+export clean!
+
+using LinearAlgebra
 
 """
-    struct SparseStateIndexed{Scalar<:Number, BR}
+    struct SparseStateIndexed{S<:Number, BR}
 
-Represents a row vector?
+Represents a row vector. Not Free. Access by Integer (1-based index)
 """
-mutable struct SparseStateIndexed{Scalar<:Number, HSR}
-  hilbert_space_realization ::HSR
-  components ::DefaultDict{Int, Scalar, Scalar}
-  function SparseStateIndexed{Scalar, HSR}(hs ::HSR) where {Scalar, HSR <:HilbertSpaceRealization}
-    return new{Scalar, HSR}(hs, DefaultDict{Int, Scalar, Scalar}(zero(Scalar)))
+mutable struct SparseStateIndexed{S<:Number, QN, BR}
+  hilbert_space_realization ::HilbertSpaceRealization{QN, BR}
+  components ::Dict{Int, S}
+
+  function SparseStateIndexed{S, QN, BR}(
+      hsr ::HilbertSpaceRealization{QN, BR},
+      components ::Dict{Int, S}) where {S, QN, BR}
+    return new{S, QN, BR}(hsr, components)
   end
 
-  function SparseStateIndexed{Scalar, HSR}(hs ::HilbertSpace, binrep ::BR) where {Scalar, BR}
-    components = DefaultDict{BR, Scalar, Scalar}(zero(Scalar))
-    components[binrep] = one(Scalar)
-    return new{Scalar, BR}(hs, components)
+  function SparseStateIndexed(
+      hsr ::HilbertSpaceRealization{QN, BR},
+      components ::Dict{Int, S}) where {S, QN, BR}
+    return new{S, QN, BR}(hsr, components)
   end
 
-  function SparseStateIndexed{Scalar, BR}(hs ::HilbertSpace, component::Pair{BR, S2}, rest...) where {Scalar, BR, S2}
-    components = DefaultDict{BR, Scalar, Scalar}(zero(Scalar))
-    components[component.first] = component.second
-    for (cf, cs) in rest
-      components[cf] = cs
-    end
-    return new{Scalar, BR}(hs, components)
+  function SparseStateIndexed{S, QN, BR}(
+      hsr ::HilbertSpaceRealization{QN, BR}) where {S, QN, BR}
+    return new{S, QN, BR}(hsr, Dict{Int, S}())
   end
 
-  function SparseStateIndexed{Scalar, BR}(hs ::HilbertSpace, components ::AbstractDict{BR, S2}) where {Scalar, BR, S2}
+  function SparseStateIndexed{S, QN, BR}(
+      hsr ::HilbertSpaceRealization{QN, BR},
+      idx ::Integer) where {S, QN, BR}
+    return new{S, QN, BR}(hsr, Dict{Int, S}(idx => one(S)))
+  end
+
+  function SparseStateIndexed{S, QN, BR}(
+      hsr ::HilbertSpaceRealization{QN, BR},
+      components::Pair{<:Integer, <:Number}...) where {S, QN, BR}
+    return new{S, QN, BR}(hsr, Dict{Int, S}(components))
+  end
+
+  function SparseStateIndexed{S, QN, BR}(
+      hsr ::HilbertSpaceRealization{QN, BR},
+      components ::AbstractDict{<:Integer, S2}) where {S, QN, BR, S2 <:Number}
     # TODO bound checking
-    return new{Scalar, BR}(hs, components)
+    return new{S, QN, BR}(hsr, Dict{Int, S}(components))
   end
 end
 
 import Base.getindex, Base.setindex!
 
-function Base.getindex(state ::SparseStateIndexed{Scalar, BR}, basis ::BR2) where {Scalar, BR, BR2}
-  # TODO: check hilbert space
-  return state.components[basis]
+@inline function Base.getindex(state ::SparseStateIndexed{S, QN, BR}, idx ::Integer) where {S, QN, BR}
+  hsr = state.hilbert_space_realization
+  @boundscheck (idx <= 0 || idx > dimension(hsr)) || throw(BoundsError("attempt to access SparseStateIndexed of dimension $(dimension(hsr)) at index [$idx]"))
+  return get(state.components, idx, zero(S))
 end
 
-function Base.setindex!(state ::SparseStateIndexed{Scalar, BR}, value ::S, basis ::BR2) where {Scalar, BR, S<:Number, BR2}
-  # TODO: check hilbert space
-  Base.setindex!(state.components, value, basis)
-  state
+function Base.setindex!(state ::SparseStateIndexed{S, QN, BR}, value ::S, idx ::Integer) where {S, QN, BR}
+  hsr = state.hilbert_space_realization
+  @boundscheck (idx <= 0 || idx > dimension(hsr)) || throw(BoundsError("attempt to access SparseStateIndexed of dimension $(dimension(hsr)) at index [$idx]"))
+  state.components[idx] = value
+  return state
 end
+
+import Base.eltype
+Base.eltype(state ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR} = S
+
+
+import Base.isempty
+isempty(psi::SparseStateIndexed{S, QN, BR}) where {S, QN, BR} = isempty(psi.components)
 
 
 import Base.==
-function (==)(lhs ::SparseStateIndexed{S1, BR}, rhs::SparseState{S2, BR}) where {S1, S2, BR}
-  return (lhs.hilbert_space === rhs.hilbert_space) && (lhs.components == rhs.components)
+function (==)(lhs ::SparseStateIndexed{S1, QN, BR}, rhs::SparseStateIndexed{S2, QN, BR}) where {S1, S2, QN, BR}
+  return ((lhs.hilbert_space_realization === rhs.hilbert_space_realization)
+          && (lhs.components == rhs.components))
 end
 
 import Base.isapprox
-function isapprox(lhs ::SparseStateIndexed{S1, BR}, rhs::SparseStateIndexed{S2, BR}; atol=sqrt(eps(Float64)), rtol=sqrt(eps(Float64))) where {S1, S2, BR}
-  if lhs.hilbert_space !== rhs.hilbert_space
+function isapprox(lhs ::SparseStateIndexed{S1, QN, BR}, rhs::SparseStateIndexed{S2, QN, BR}; atol=sqrt(eps(Float64)), rtol=sqrt(eps(Float64))) where {S1, S2, QN, BR}
+  if lhs.hilbert_space_realization !== rhs.hilbert_space_realization
     return false
   end
-
   all_keys = union(keys(lhs.components), keys(rhs.components))
   for k in all_keys
-    lv = haskey(lhs.components, k) ? lhs.components[k] : zero(S1)
-    rv = haskey(rhs.components, k) ? rhs.components[k] : zero(S2)
-    if ! isapprox(lv, rv; atol=atol, rtol=rtol)
-      return false
-    end
+    lv = get(lhs.components, k, zero(S1))
+    rv = get(rhs.components, k, zero(S2))
+    isapprox(lv, rv; atol=atol, rtol=rtol) || return false
   end
-
   return true
 end
 
 import Base.copy
-function copy(arg ::SparseStateIndexed{S, BR}) where {S, BR}
-  return SparseState{S, BR}(arg.hilbert_space, copy(arg.components))
+function copy(arg ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR}
+  return SparseStateIndexed{S, BR}(arg.hilbert_space_realization, copy(arg.components))
 end
 
 import Base.real, Base.imag, Base.conj
-real(arg ::SparseStateIndexed{R, BR}) where {R<:Real, BR} = arg
-imag(arg ::SparseStateIndexed{R, BR}) where {R<:Real, BR} = SparseStateIndexed{R, BR}(arg.hilbert_space)
-conj(arg ::SparseStateIndexed{R, BR}) where {R<:Real, BR} = arg
+real(arg ::SparseStateIndexed{R, QN, BR}) where {R<:Real, QN, BR} = copy(arg)
+imag(arg ::SparseStateIndexed{R, QN, BR}) where {R<:Real, QN, BR} = SparseStateIndexed{R, QN, BR}(arg.hilbert_space_realization)
+conj(arg ::SparseStateIndexed{R, QN, BR}) where {R<:Real, QN, BR} = copy(arg)
 
-function real(arg ::SparseStateIndexed{Complex{R}, BR}) where {R<:Real, BR}
-  return SparseState{R, BR}(arg.hilbert_space,
-                            DefaultDict{BR, R, R}(zero(R),
-                                                  [(k, real(v)) for (k, v) in arg.components]
-                                                 )
-                           )
+function real(arg ::SparseStateIndexed{Complex{R}, QN, BR}) where {R<:Real, QN, BR}
+  hsr = arg.hilbert_space_realization
+  components = Dict{Int, R}((k, real(v)) for (k, v) in arg.components)
+  return SparseStateIndexed{R, QN, BR}(hsr, components)
 end
 
-function imag(arg ::SparseStateIndexed{Complex{R}, BR}) where {R<:Real, BR}
-  return SparseStateIndexed{R, BR}(arg.hilbert_space,
-                            DefaultDict{BR, R, R}(zero(R),
-                                                  [(k, imag(v)) for (k, v) in arg.components]
-                                                 )
-                           )
+function imag(arg ::SparseStateIndexed{Complex{R}, QN, BR}) where {R<:Real, QN, BR}
+  hsr = arg.hilbert_space_realization
+  components = Dict{Int, R}((k, imag(v)) for (k, v) in arg.components)
+  return SparseStateIndexed{R, QN, BR}(hsr, components)
 end
 
-function conj(arg ::SparseStateIndexed{Complex{R}, BR}) where {R<:Real, BR}
-  return SparseState{Complex{R}, BR}(arg.hilbert_space,
-                                     SparseStateIndexed{BR, Complex{R}, Complex{R}}(zero(Complex{R}),
-                                                                             [(k, conj(v)) for (k, v) in arg.components]
-                                                                            )
-                                    )
+function conj(arg ::SparseStateIndexed{Complex{R}, QN, BR}) where {R<:Real, QN, BR}
+  C = Complex{R}
+  hsr = arg.hilbert_space_realization
+  components = Dict{Int, C}((k, conj(v)) for (k, v) in arg.components)
+  return SparseStateIndexed{C, QN, BR}(hsr, components)
 end
 
-import Base.-, Base.+, Base.*, Base./
+import Base.-, Base.+, Base.*, Base./, Base.\
 
-(+)(arg ::SparseStateIndexed{S, BR}) where {S, BR} = copy(arg)
+(+)(arg ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR} = copy(arg)
 
-function (-)(arg ::SparseStateIndexed{S, BR}) where {S, BR}
-  out = SparseStateIndexed{S, BR}(arg.hilbert_space)
-  for (b, v) in arg.components
-    out[b] = -v
-  end
-  return out
+function (-)(arg ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR}
+  return SparseStateIndexed{S, QN, BR}(arg.hilbert_space_realization,
+                                       Dict{Int, S}((k, -v) for (k, v) in arg.components))
 end
 
-function (+)(lhs ::SparseStateIndexed{S1, BR}, rhs ::SparseStateIndexed{S2, BR}) where {S1, S2, BR}
-  S3 = promote_type(S1, S2)
-  if lhs.hilbert_space !== rhs.hilbert_space
+function (+)(lhs ::SparseStateIndexed{S1, QN, BR}, rhs ::SparseStateIndexed{S2, QN, BR}) where {S1, S2, QN, BR}
+  if lhs.hilbert_space_realization !== rhs.hilbert_space_realization
     throw(ArgumentError("Hilbert spaces of lhs and rhs of + should match"))
   end
-  out = SparseStateIndexed{S3, BR}(lhs.hilbert_space)
-  for (b, v) in lhs.components
-    out[b] = v
+  S3 = promote_type(S1, S2)
+  hsr = arg.hilbert_space_realization
+  components = Dict{Int, S3}((k, S3(v)) for (k, v) in lhs.components)
+  zero_value = zero(S3)
+  for (i, v) in rhs.components
+    components[i] = get(components, i, zero_value) + v
   end
-  for (b, v) in rhs.components
-    out[b] += v
-  end
-  return out
+  return SparseStateIndexed{S, QN, BR}(hsr, components)
 end
 
-function (-)(lhs ::SparseStateIndexed{S1, BR}, rhs ::SparseStateIndexed{S2, BR}) where {S1, S2, BR}
-  S3 = promote_type(S1, S2)
-  if lhs.hilbert_space !== rhs.hilbert_space
+function (-)(lhs ::SparseStateIndexed{S1, QN, BR}, rhs ::SparseStateIndexed{S2, QN, BR}) where {S1, S2, QN, BR}
+  if lhs.hilbert_space_realization !== rhs.hilbert_space_realization
     throw(ArgumentError("Hilbert spaces of lhs and rhs of + should match"))
   end
-  out = SparseStateIndexed{S3, BR}(lhs.hilbert_space)
-  for (b, v) in lhs.components
-    out[b] = v
+  S3 = promote_type(S1, S2)
+  hsr = arg.hilbert_space_realization
+  components = Dict{Int, S3}((k, S3(v)) for (k, v) in lhs.components)
+  zero_value = zero(S3)
+  for (i, v) in rhs.components
+    components[i] = get(components, i, zero_value) - v
   end
-  for (b, v) in rhs.components
-    out[b] -= v
-  end
-  return out
+  return SparseStateIndexed{S, QN, BR}(hsr, components)
 end
 
-function (*)(lhs ::SparseStateIndexed{S1, BR}, rhs ::S2) where {S1, S2<:Number, BR}
-  S3 = promote_type(S1, S2)
-  out = SparseState{S3, BR}(lhs.hilbert_space)
-  for (b, v) in lhs.components
-    out[b] = v * rhs
-  end
-  return out
+function (*)(lhs ::SparseStateIndexed{S1, QN, BR}, rhs ::S2) where {S1, S2<:Number, QN, BR}
+  hsr = lhs.hilbert_space_realization
+  return SparseStateIndexed(hsr, Dict((k, v * rhs) for (k, v) in lhs.components))
 end
 
-function (*)(lhs ::S1, rhs ::SparseStateIndexed{S2, BR}) where {S1<:Number, S2<:Number, BR}
-  S3 = promote_type(S1, S2)
-  out = SparseStateIndexed{S3, BR}(rhs.hilbert_space)
-  for (b, v) in rhs.components
-    out[b] = lhs * v
-  end
-  return out
+function (*)(lhs ::S1, rhs ::SparseStateIndexed{S2, QN, BR}) where {S1<:Number, S2<:Number, QN, BR}
+  hsr = rhs.hilbert_space_realization
+  return SparseStateIndexed(hsr, Dict((k, lhs * v) for (k, v) in rhs.components))
 end
 
-function (/)(lhs ::SparseStateIndexed{S1, BR}, rhs ::S2) where {S1, S2<:Number, BR}
-  S3 = promote_type(S1, S2)
-  out = SparseStateIndexed{S3, BR}(lhs.hilbert_space)
-  for (b, v) in lhs.components
-    out[b] = v / rhs
-  end
-  return out
+function (/)(lhs ::SparseStateIndexed{S1, QN, BR}, rhs ::S2) where {S1, S2<:Number, QN, BR}
+  hsr = lhs.hilbert_space_realization
+  return SparseStateIndexed(hsr, Dict((k, v / rhs) for (k, v) in lhs.components))
+end
+
+function (\)(lhs ::S1, rhs ::SparseStateIndexed{S2, QN, BR}) where {S1<:Number, S2<:Number, QN, BR}
+  hsr = rhs.hilbert_space_realization
+  return SparseStateIndexed(hsr, Dict((k, lhs \ v) for (k, v) in rhs.components))
 end
 
 import Base.convert
-function convert(type ::Type{SparseState{S1, BR}}, obj::SparseStateIndexed{S2, BR}) where {S1, S2, BR}
-  state = SparseStateIndexed{S1, BR}(obj.hilbert_space)
-  for (k, v) in obj.components
-    state[k] = v
-  end
-  return state
+function convert(type ::Type{SparseStateIndexed{S1, QN, BR}},
+                 obj::SparseStateIndexed{S2, QN, BR}) where {S1, S2, QN, BR}
+  return SparseStateIndexed{S1, QN, BR}(obj.hilbert_space_realization, Dict{Int, S1}(obj.components))
 end
 
 
-function clean!(arg ::SparseStateIndexed{S1, BR}; tol=sqrt(eps(Float64))) where {S1, BR}
+import Base.iterate
+Base.iterate(arg ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR} = Base.iterate(arg.components)
+Base.iterate(arg ::SparseStateIndexed{S, QN, BR}, i::Int) where {S, QN, BR} = Base.iterate(arg.components, i)
+
+function clean!(arg ::SparseStateIndexed{S, QN, BR}; tol=sqrt(eps(Float64))) where {S, QN, BR}
   to_delete = [k for (k, v) in arg.components if isapprox(v, 0; atol=tol)]
   for k in to_delete
     delete!(arg.components, k)
   end
 end
 
-
 import LinearAlgebra.norm
-function norm(arg ::SparseStateIndexed{S1, BR}) where {S1, BR}
+function norm(arg ::SparseStateIndexed{S, QN, BR}) where {S, QN, BR}
   if isempty(arg.components)
-    return zero(real(S1))
+    return zero(real(S))
   else
     return norm(values(arg.components))
   end
 end
 
 import LinearAlgebra.normalize
-function normalize(arg ::SparseStateIndexed{S1, BR}) where {S1, BR}
+function normalize(arg ::SparseStateIndexed{S, BR}) where {S, QN, BR}
   norm_val = norm(arg)
-  S2 = promote_type(typeof(norm_val), S1)
-  components = DefaultDict{BR, S2, S2}(zero(S2), [(k, v/norm_val) for (k, v) in arg.components])
+  components = Dict((k, v/norm_val) for (k, v) in arg.components)
   return SparseStateIndexed{S2, BR}(arg.hilbert_space, components)
 end
 
 import LinearAlgebra.normalize!
-function normalize!(arg ::SparseStateIndexed{S1, BR}) where {S1, BR}
+function normalize!(arg ::SparseStateIndexed{S, BR}) where {S, QN, BR}
   norm_val = norm(arg)
   for (k, v) in arg.components
     arg[k] = v / norm_val
   end
   arg
 end
+
+
+# TODO Broadcasting
