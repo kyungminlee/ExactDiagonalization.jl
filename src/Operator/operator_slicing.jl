dd53362cb1b0f379cd51ba074d6fbfbabf99dce2
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-22 01:42:43 -0400

    Let slicing of pureop use references to operators

--- src/Operator/operator_slicing.jl
+++ src/Operator/operator_slicing.jl
@@ -12,25 +12,26 @@ end
   match(b ::BR) ::Bool = (b & pureop.bitmask) == pureop.bitcol
   element(b ::BR) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
   return (element(b) for b in [bcol] if match(b))
-  # if (bcol & pureop.bitmask) == pureop.bitcol
-  #   brow = (bcol & ~pureop.bitmask) | pureop.bitrow
-  #   return Tuple{BR, S}[(brow, pureop.amplitude)]
-  # else
-  #   return Tuple{BR, S}[]
-  # end
+  # bm = pureop.bitmask
+  # br = pureop.bitrow
+  # bc = pureop.bitcol
+  # am = pureop.amplitude
+  # match(b ::BR) ::Bool = (b & bm) == bc
+  # element(b ::BR) ::Pair{BR, S} = (((b & ~bm) | br) => am)
+  # return (element(b) for b in [bcol] if match(b))
 end
 
 @inline function get_slice(brow ::BR, pureop ::PureOperator{S, BR}) where {S, BR}
   match(b ::BR) ::Bool = (b & pureop.bitmask) == pureop.bitrow
   element(b ::BR) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitcol) => pureop.amplitude)
   return (element(b) for b in [brow] if match(b))
-  #
-  # if (brow & pureop.bitmask) == pureop.bitrow
-  #   bcol = (brow & ~pureop.bitmask) | pureop.bitcol
-  #   return Tuple{BR, S}[(bcol, pureop.amplitude)]
-  # else
-  #   return Tuple{BR, S}[]
-  # end
+  # bm = pureop.bitmask
+  # br = pureop.bitrow
+  # bc = pureop.bitcol
+  # am = pureop.amplitude
+  # match(b ::BR) ::Bool = (b & bm) == br
+  # element(b ::BR) ::Pair{BR, S} = (((b & ~bm) | bc) => am)
+  # return (element(b) for b in [brow] if match(b))
 end
 
 @inline function get_slice(sumop ::SumOperator{S, BR}, bcol ::BR) where {S, BR}
