ca356ef454e9d08df6ea50e82c49bd54d7aac0ee
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-24 16:00:18 -0400

    Add test for HSS

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using ExactDiagonalization
 include("test_util.jl")
 
 include("test_hilbert_space.jl")
+include("test_hilbert_space_sector.jl")
 #include("test_sparse_state.jl")
 include("test_operator.jl")
 
