8880800636198a2fa325bbb5a10f48cb8529510f
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-24 17:47:05 -0400

    Update test
    - fix hilbert space representation tests
    - add operator representation test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ include("test_util.jl")
 
 include("test_hilbert_space.jl")
 include("test_hilbert_space_sector.jl")
-include("test_hilbert_space_realization.jl")
-#include("test_sparse_state.jl")
 include("test_operator.jl")
-
-include("test_hilbert_space_realization.jl")
-include("test_operator_application.jl")
+#include("test_sparse_state.jl")
 include("test_operator_simplify.jl")
+include("test_operator_application.jl")
+
+include("test_hilbert_space_representation.jl")
+include("test_operator_representation.jl")
 
 include("test_prettyprintln.jl")
---
+++ test/test_operator_representation.jl
@@ -0,0 +1,5 @@
+using Test
+using ExactDiagonalization
+
+using StaticArrays
+using LinearAlgebra
