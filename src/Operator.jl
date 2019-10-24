b7de7a6f2436aa533af9d6c1f9c14c320cdc418c
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-24 14:10:17 -0400

    Fix operator
    operator_iterator.jl instead of operator_slicing.jl

--- src/Operator.jl
+++ src/Operator.jl
@@ -5,6 +5,6 @@ include("Operator/pureoperator.jl")
 include("Operator/sumoperator.jl")
 
 include("Operator/operator_simplify.jl")
-include("Operator/operator_slicing.jl")
+include("Operator/operator_iterator.jl")
 
 include("Operator/apply_sparse_state.jl")
