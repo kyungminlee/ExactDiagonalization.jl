ed54beaaddde053296958482820ede801391796d
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-25 12:10:53 -0400

    Add back tests for sparse state

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("test_util.jl")
 include("test_hilbert_space.jl")
 include("test_hilbert_space_sector.jl")
 include("test_operator.jl")
-#include("test_sparse_state.jl")
+include("test_sparse_state.jl")
 include("test_operator_simplify.jl")
 include("test_operator_application.jl")
 
