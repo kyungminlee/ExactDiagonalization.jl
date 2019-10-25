9f30e15aa467302ace81ea2d4ab8495c29f1e212
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-25 00:57:39 -0400

    Add version check for tests

--- test/test_operator.jl
+++ test/test_operator.jl
@@ -4,14 +4,15 @@ using ExactDiagonalization
 using LinearAlgebra
 using StaticArrays
 
-@testset "AbstractOperator" begin
-  struct Foo <:AbstractOperator
+if VERSION >= v"1.1" # local type definition
+  @testset "AbstractOperator" begin
+    struct Foo <:AbstractOperator
+    end
+    x = Foo()
+    @test_throws ErrorException get_row_iterator(x, 0x1)
+    @test_throws ErrorException get_column_iterator(x, 0x1)
   end
-  x = Foo()
-  @test_throws ErrorException get_row_iterator(x, 0x1)
-  @test_throws ErrorException get_column_iterator(x, 0x1)
 end
-
 
 @testset "NullOperator" begin
   nop = NullOperator()
