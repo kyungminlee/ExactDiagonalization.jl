b05bcfe0c44cea7577fbede560d802e3574ee67a
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-25 16:22:44 -0400

    Update test for OpRep

--- test/test_operator_representation.jl
+++ test/test_operator_representation.jl
@@ -155,13 +155,13 @@ end;
 
       @testset "single-threaded" begin
         opr = represent(hsr_0, σ[2, :+])
-        
+
         σ₊ = [0 1; 0 0]
         σ₀ = [1 0; 0 1]
-        
+
         op_dense = kron(σ₀, σ₀, σ₊, σ₀)
         state = rand(ComplexF64, dim)
-        
+
         out1 = zeros(ComplexF64, dim)
 
         # matrix * columnvector
@@ -170,14 +170,14 @@ end;
         out2 = opr * state
         @test isapprox(out0, out1, atol=1E-6)
         @test isapprox(out0, out2, atol=1E-6)
-        
+
         # add to the previous (do not overwrite)
         apply!(out1, opr, state)
         @test !isapprox(out0, out1, atol=1E-6)
-        
+
         # rowvector * matrix
         out1[:] .= zero(ComplexF64)
-        
+
         out0 = transpose( transpose(state) * op_dense )
         apply!(out1, state, opr)
         out2 = state * opr
@@ -194,28 +194,29 @@ end;
       end
 
       @testset "multi-threaded" begin
-        opr = represent(hsr_0, σ[2, :+])
-        
+        opr = represent(hsr_0, σ[1, :+] * σ[4, :-])
+
         σ₊ = [0 1; 0 0]
+        σ₋ = [0 0; 1 0]
         σ₀ = [1 0; 0 1]
-        
-        op_dense = kron(σ₀, σ₀, σ₊, σ₀)
+
+        op_dense = kron(σ₋, σ₀, σ₀, σ₊)
         state = rand(ComplexF64, dim)
-        
+
         out1 = zeros(ComplexF64, dim)
 
         # matrix * columnvector
         out0 = op_dense * state
         apply_threaded!(out1, opr, state)
         @test isapprox(out0, out1, atol=1E-6)
-        
+
         # add to the previous (do not overwrite)
         apply_threaded!(out1, opr, state)
         @test !isapprox(out0, out1, atol=1E-6)
-        
+
         # rowvector * matrix
         out1[:] .= zero(ComplexF64)
-        
+
         out0 = transpose( transpose(state) * op_dense )
         apply_threaded!(out1, state, opr)
         @test isapprox(out0, out1, atol=1E-6)
@@ -234,14 +235,14 @@ end;
         opr = represent(hsr_small, σ[2, :x])
         dim_small = dimension(hsr_small)
         state = 2im*ones(ComplexF64, dim_small)
-        
+
         out1 = zeros(ComplexF64, dim_small)
         e1, e2 = apply!(out1, opr, state)
         tol = sqrt(eps(Float64))
         @test isapprox(e1, 12im; atol=tol)
         @test isapprox(e2, 24.0; atol=tol)
         @test all(isapprox.(out1, 0; atol=tol))
-        
+
         out1[:] .= zero(ComplexF64)
         e1, e2 = apply_threaded!(out1, opr, state)
         @test isapprox(e1, 12im; atol=tol)
