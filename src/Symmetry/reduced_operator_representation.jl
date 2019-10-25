603e2dd1f4e3e7feb810596a5e58035b6fa1857b
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-25 00:11:54 -0400

    Change realization to representation for RedOpRep

--- src/Symmetry/reduced_operator_representation.jl
+++ src/Symmetry/reduced_operator_representation.jl
@@ -7,13 +7,13 @@ export get_row
 export get_column
 
 struct ReducedOperatorRepresentation{RHSR <:ReducedHilbertSpaceRepresentation, O <:AbstractOperator} <:AbstractOperatorRepresentation
-  reduced_hilbert_space_realization ::RHSR
+  reduced_hilbert_space_representation ::RHSR
   operator ::O
 end
 
 @inline spacetype(lhs::Type{ReducedOperatorRepresentation{RHSR, O}}) where {RHSR, O} = RHSR
 @inline operatortype(lhs ::Type{ReducedOperatorRepresentation{RHSR, O}}) where {RHSR, O} = O
-@inline get_space(lhs ::ReducedOperatorRepresentation{RHSR, O}) where {RHSR, O} = lhs.reduced_hilbert_space_realization
+@inline get_space(lhs ::ReducedOperatorRepresentation{RHSR, O}) where {RHSR, O} = lhs.reduced_hilbert_space_representation
 
 function represent(rhsr ::RHSR, op ::O) where {RHSR <:ReducedHilbertSpaceRepresentation, O <:AbstractOperator}
     return ReducedOperatorRepresentation{RHSR, O}(rhsr, op)
@@ -23,10 +23,10 @@ end
 function get_row_iterator(opr ::ReducedOperatorRepresentation{RHSR, O},
                           irow_r ::Integer;
                           include_all::Bool=false) where {RHSR, O}
-  rhsr = opr.reduced_hilbert_space_realization
+  rhsr = opr.reduced_hilbert_space_representation
   hsr = rhsr.parent
   S = eltype(opr)
-  dim = dimension(opr.reduced_hilbert_space_realization)
+  dim = dimension(opr.reduced_hilbert_space_representation)
 
   brow = rhsr.basis_list[irow_r]
   irow_p = hsr.basis_lookup[brow]
@@ -34,9 +34,9 @@ function get_row_iterator(opr ::ReducedOperatorRepresentation{RHSR, O},
   @assert irow_r == irow_r2 "$irow_r != $irow_r2"
 
   zero_val = zero(S)
-  missing_val = (-1 => zero_val)
+  missing_val ::Pair{Int, S} = (-1 => zero_val)
 
-  function element(bcol, ampl)
+  function element(bcol, ampl) ::Pair{Int, S}
     haskey(rhsr.parent.basis_lookup, bcol) || return missing_val
     icol_p = rhsr.parent.basis_lookup[bcol]
     (icol_r, ampl_col) = rhsr.basis_mapping[icol_p]
@@ -55,10 +55,10 @@ end
 
 function get_column_iterator(opr ::ReducedOperatorRepresentation{RHSR, O}, icol_r ::Integer;
                              include_all::Bool=false) where {RHSR, O}
-  rhsr = opr.reduced_hilbert_space_realization
+  rhsr = opr.reduced_hilbert_space_representation
   hsr = rhsr.parent
   S = eltype(opr)
-  dim = dimension(opr.reduced_hilbert_space_realization)
+  dim = dimension(opr.reduced_hilbert_space_representation)
 
   bcol = rhsr.basis_list[icol_r]
   icol_p = hsr.basis_lookup[bcol]
@@ -66,9 +66,9 @@ function get_column_iterator(opr ::ReducedOperatorRepresentation{RHSR, O}, icol_
   @assert icol_r == icol_r2 "$icol_r != $icol_r2"
 
   zero_val = zero(S)
-  missing_val = (-1 => zero_val)
+  missing_val ::Pair{Int, S} = (-1 => zero_val)
 
-  function element(brow, ampl)
+  function element(brow, ampl) ::Pair{Int, S}
     haskey(rhsr.parent.basis_lookup, brow) || return missing_val
     irow_p = rhsr.parent.basis_lookup[brow]
     (irow_r, ampl_row) = rhsr.basis_mapping[irow_p]
