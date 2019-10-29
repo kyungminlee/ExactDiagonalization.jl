df0e8a93aa814f003f235cd89eb2d2c97c90d7b0
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-29 11:14:11 -0400

    Move `splitblock` and `splitrange` to `util.jl`

--- src/Representation/operator_representation.jl
+++ src/Representation/operator_representation.jl
@@ -183,31 +183,6 @@ function apply_serial!(out ::Vector{S1},
 end
 
 
-"""
-    splitblock
-
-Split n into b blocks.
-
-# Arguments
-- `n ::Integer`: the number of elements to split.
-- `b ::Integer`: the number of blocks.
-"""
-function splitblock(n ::Integer, b ::Integer) ::Vector{Int}
-  (n < 0) && throw(ArgumentError("n cannot be negative"))
-  (b <= 0) && throw(ArgumentError("b must be positive"))
-  blocksize = n ÷ b
-  blocks = blocksize * ones(Int, b)
-  r = n - (blocksize * b)
-  blocks[1:r] .+= 1
-  return blocks
-end
-
-function splitrange(range::AbstractVector{<:Integer}, b::Integer)
-  nblocks = b
-  block_sizes = splitblock(length(range), nblocks)
-  block_offsets = cumsum([1, block_sizes...])
-  return [range[block_offsets[iblock]:block_offsets[iblock+1]-1] for iblock in 1:nblocks]
-end
 
 """
     apply_parallel!(out, opr, state; range=1:size(opr, 2))
--- src/util.jl
+++ src/util.jl
@@ -43,3 +43,31 @@ function choptol!(d ::Dict{K, V}, tol::Real) where {K, V<:Number}
   end
   d
 end
+
+
+
+"""
+    splitblock
+
+Split n into b blocks.
+
+# Arguments
+- `n ::Integer`: the number of elements to split.
+- `b ::Integer`: the number of blocks.
+"""
+function splitblock(n ::Integer, b ::Integer) ::Vector{Int}
+  (n < 0) && throw(ArgumentError("n cannot be negative"))
+  (b <= 0) && throw(ArgumentError("b must be positive"))
+  blocksize = n ÷ b
+  blocks = blocksize * ones(Int, b)
+  r = n - (blocksize * b)
+  blocks[1:r] .+= 1
+  return blocks
+end
+
+function splitrange(range::AbstractVector{<:Integer}, b::Integer)
+  nblocks = b
+  block_sizes = splitblock(length(range), nblocks)
+  block_offsets = cumsum([1, block_sizes...])
+  return [range[block_offsets[iblock]:block_offsets[iblock+1]-1] for iblock in 1:nblocks]
+end
