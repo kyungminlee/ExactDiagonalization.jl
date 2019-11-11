163346a9fd70e993f7fbf64caca1e0840bd892c7
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-11-11 12:47:13 -0500

    Update operator_iterator to reduce allocation

--- src/Operator/operator_iterator.jl
+++ src/Operator/operator_iterator.jl
@@ -3,39 +3,35 @@ export get_column_iterator
 export get_element
 
 @inline function get_row_iterator(nullop ::NullOperator, br ::BR) where {BR<:Unsigned}
-  return Pair{Bool, Bool}[]
+  return (zero(BR) => false for i in 1:0)
 end
 
 @inline function get_column_iterator(nullop ::NullOperator, bc ::BR) where {BR<:Unsigned}
-  return Pair{Bool, Bool}[]
+  return (zero(BR) => false for i in 1:0)
 end
 
-@inline function get_column_iterator(pureop ::PureOperator{S, BR}, bcol ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
-  match(b ::BR2) ::Bool = (b & pureop.bitmask) == pureop.bitcol
-  element(b ::BR2) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
-  #range = match(bcol) ? (1:1) : (1:0)
-  #return (element(bcol) for x in range)
-  return match(bcol) ? Pair{BR, S}[element(bcol)] : Pair{BR, S}[]
+function get_column_iterator(pureop ::PureOperator{S, BR}, bcol ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
+  match ::Bool = (bcol & pureop.bitmask) == pureop.bitcol
+  element ::Pair{BR, S} = (((bcol & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
+  return (element for i in 1:(match ? 1 : 0))
 end
 
-@inline function get_row_iterator(pureop ::PureOperator{S, BR}, brow ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
-  match(b ::BR2) ::Bool = (b & pureop.bitmask) == pureop.bitrow
-  element(b ::BR2) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitcol) => pureop.amplitude)
-  return match(brow) ? Pair{BR, S}[element(brow)] : Pair{BR, S}[]
+function get_row_iterator(pureop ::PureOperator{S, BR}, brow ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
+  match ::Bool = (brow & pureop.bitmask) == pureop.bitrow
+  element ::Pair{BR, S} = (((brow & ~pureop.bitmask) | pureop.bitcol) => pureop.amplitude)
+  return (element for i in 1:(match ? 1 : 0))
 end
 
-@inline function get_column_iterator(sumop ::SumOperator{S, BR}, bcol ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
-  match(pureop::PureOperator{S, BR}, b ::BR2) ::Bool = (b & pureop.bitmask) == pureop.bitcol
-  element(pureop::PureOperator{S, BR}, b ::BR2) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
-  return (element(t, bcol) for t in sumop.terms if match(t, bcol))
-  #return Base.Iterators.flatten(get_column_iterator(t, bcol) for t in sumop.terms)
+function get_column_iterator(sumop ::SumOperator{S, BR}, bcol ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
+  match(pureop::PureOperator{S, BR}) ::Bool = (bcol & pureop.bitmask) == pureop.bitcol
+  element(pureop::PureOperator{S, BR}) ::Pair{BR, S} = (((bcol & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
+  return (element(t) for t in sumop.terms if match(t))
 end
 
-@inline function get_row_iterator(sumop::SumOperator{S, BR}, brow ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
-  match(pureop::PureOperator{S, BR}, b ::BR2) ::Bool = (b & pureop.bitmask) == pureop.bitrow
-  element(pureop::PureOperator{S, BR}, b ::BR2) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitcol) => pureop.amplitude)
-  return (element(t, brow) for t in sumop.terms if match(t, brow))
-  #return Base.Iterators.flatten(get_row_iterator(t, brow) for t in sumop.terms)
+function get_row_iterator(sumop::SumOperator{S, BR}, brow ::BR2) where {S, BR<:Unsigned, BR2<:Unsigned}
+  match(pureop::PureOperator{S, BR}) ::Bool = (brow & pureop.bitmask) == pureop.bitrow
+  element(pureop::PureOperator{S, BR}) ::Pair{BR, S} = (((brow & ~pureop.bitmask) | pureop.bitcol) => pureop.amplitude)
+  return (element(t) for t in sumop.terms if match(t))
 end
 
 @inline function get_element(nullop::NullOperator, br ::BR2, bc::BR3) ::Bool where {BR2 <:Unsigned, BR3<:Unsigned}
@@ -43,8 +39,6 @@ end
 end
 
 @inline function get_element(pureop::PureOperator{S, BR}, br ::BR2, bc::BR3) ::S where {S, BR, BR2 <:Unsigned, BR3<:Unsigned}
-  match(b ::BR2) ::Bool = (b & pureop.bitmask) == pureop.bitcol
-  element(b ::BR2) ::Pair{BR, S} = (((b & ~pureop.bitmask) | pureop.bitrow) => pureop.amplitude)
   if ((br & pureop.bitmask) == pureop.bitrow) && ((bc & pureop.bitmask) == pureop.bitcol)
     return pureop.amplitude
   else
@@ -52,6 +46,6 @@ end
   end
 end
 
-@inline function get_element(sumop::SumOperator{S, BR}, br ::BR2, bc::BR3) where {S, BR, BR2 <:Unsigned, BR3<:Unsigned}
+function get_element(sumop::SumOperator{S, BR}, br ::BR2, bc::BR3) where {S, BR, BR2 <:Unsigned, BR3<:Unsigned}
   return sum(get_element(op, br, bc)::S for op in sumop.terms)
 end
