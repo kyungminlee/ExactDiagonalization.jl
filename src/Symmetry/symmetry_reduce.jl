987920b0d307c58c65547d99df7cf0a0c5e85721
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-11-20 14:23:40 -0500

    Replace Memento with Logging

--- src/Symmetry/symmetry_reduce.jl
+++ src/Symmetry/symmetry_reduce.jl
@@ -121,7 +121,7 @@ function symmetry_reduce_parallel(
     tol::Real=sqrt(eps(Float64))) where {QN, BR, DT}
 
   HSR = HilbertSpaceRepresentation{QN, BR, DT}
-  debug(LOGGER, "BEGIN symmetry_reduce_parallel")
+  @debug "BEGIN symmetry_reduce_parallel"
   ik = let
     match(k ::Vector{Rational{Int}}) ::Bool  = k == fractional_momentum
     findfirst(match, trans_group.fractional_momenta)
@@ -133,7 +133,7 @@ function symmetry_reduce_parallel(
   end
   phases = conj.(trans_group.character_table[ik, :])
   n_basis = length(hsr.basis_list)
-  debug(LOGGER, "Original Hilbert space dimension: $n_basis")
+  @debug "Original Hilbert space dimension: $n_basis"
 
   visit_lock = Threads.SpinLock()
   nthreads = Threads.nthreads()
@@ -146,7 +146,7 @@ function symmetry_reduce_parallel(
     denom = max(1, length(trans_group.fractional_momenta) - 1)
     n_basis ÷ denom
   end
-  debug(LOGGER, "Estimate for the reduced Hilbert space dimension: $size_estimate")
+  @debug "Estimate for the reduced Hilbert space dimension: $size_estimate"
   for i in eachindex(local_reduced_basis_list)
     sizehint!(local_reduced_basis_list[i], size_estimate ÷ nthreads + 1)
   end
@@ -166,7 +166,7 @@ function symmetry_reduce_parallel(
   prev_progress_time = Dates.now()
 
   visited = falses(n_basis)
-  debug(LOGGER, "Starting reduction (parallel)")
+  @debug "Starting reduction (parallel)"
   Threads.@threads for itemp in 1:n_basis
     ivec_p = reorder[itemp]
     visited[ivec_p] && continue
@@ -175,7 +175,7 @@ function symmetry_reduce_parallel(
 
     compatible = true
     ψ = Dict{BR, ComplexType}()
-    
+
     for i in 1:length(trans_group.elements)
       t = trans_group.translations[i]
       g = trans_group.elements[i]
@@ -218,21 +218,21 @@ function symmetry_reduce_parallel(
       representative_amplitude_list[ivec_p_prime] = (representative=ivec_p, amplitude=amplitude / norm)
     end
   end
-  debug(LOGGER, "Finished reduction (parallel)")
+  @debug "Finished reduction (parallel)"
 
-  debug(LOGGER, "Collecting basis list")
+  @debug "Collecting basis list"
   reduced_basis_list = BR[]
   sizehint!(reduced_basis_list, sum(length(x) for x in local_reduced_basis_list))
   while !isempty(local_reduced_basis_list)
     lbl = pop!(local_reduced_basis_list)
     append!(reduced_basis_list, lbl)
   end
-  debug(LOGGER, "Sorting basis list")
+  @debug "Sorting basis list"
   sort!(reduced_basis_list)
 
   ItemType = NamedTuple{(:index, :amplitude), Tuple{Int, ComplexType}}
   basis_mapping = ItemType[(index=-1, amplitude=zero(ComplexType)) for i in hsr.basis_list]
-  debug(LOGGER, "Collecting basis lookup (diagonal)")
+  @debug "Collecting basis lookup (diagonal)"
   Threads.@threads for ivec_r in eachindex(reduced_basis_list)
     bvec = reduced_basis_list[ivec_r]
     ivec_p = hsr.basis_lookup[bvec]
@@ -240,7 +240,7 @@ function symmetry_reduce_parallel(
     basis_mapping[ivec_p] = (index=ivec_r, amplitude=amplitude)
   end
 
-  debug(LOGGER, "Collecting basis lookup (offdiagonal)")
+  @debug "Collecting basis lookup (offdiagonal)"
   Threads.@threads for ivec_p_prime in eachindex(representative_amplitude_list)
     ivec_p, amplitude = representative_amplitude_list[ivec_p_prime]
     (ivec_p == -1) && continue  # not in this irrep
@@ -250,7 +250,7 @@ function symmetry_reduce_parallel(
     @assert ivec_r > 0 "ivec_r $ivec_r <= 0"
     basis_mapping[ivec_p_prime] = (index=ivec_r, amplitude=amplitude)
   end
-  debug(LOGGER, "END symmetry_reduce_parallel")
+  @debug "END symmetry_reduce_parallel"
   return ReducedHilbertSpaceRepresentation{HSR, BR, ComplexType}(hsr, trans_group, reduced_basis_list, basis_mapping)
 end
 
