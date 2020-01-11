3356ac068b6653c05ee9d1ac185db83a51dacef9
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-01-11 15:02:07 -0500

    Add tests for Toolkit product_state

--- test/test_toolkit.jl
+++ test/test_toolkit.jl
@@ -26,9 +26,32 @@ using ExactDiagonalization
 
   @testset "product_state" begin
     (hs, pauli) = ExactDiagonalization.Toolkit.spin_half_system(4)
+    @test_throws ArgumentError ExactDiagonalization.Toolkit.product_state(hs, [[1.0, 0.0], [0.0, 0.0]]) # too few
+    @test_throws ArgumentError ExactDiagonalization.Toolkit.product_state(hs, [[1.0, 0.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0]]) # too many
+    @test_throws ArgumentError ExactDiagonalization.Toolkit.product_state(hs, [[1.0, 0.0], [0.0, 1.0], [0.0, 1.0, 0.0], [0.0, 1.0]])
+
     local_states = [[1.0, 0.0], [1.0 + 10.0im, 2.0], [1.0, 0.0], [0.0, 1.0]]
-    psi1 = ExactDiagonalization.Toolkit.product_state(hs, local_states)
-    psi2 = SparseState{ComplexF64, UInt}(0b1000 => 1.0 + 10.0im, 0b1010 => 2.0)
-    @test isapprox(psi1, psi2; atol=1E-8)
+    @testset "HilbertSpace" begin
+      psi1 = ExactDiagonalization.Toolkit.product_state(hs, local_states)
+      psi2 = SparseState{ComplexF64, UInt}(0b1000 => 1.0 + 10.0im, 0b1010 => 2.0)
+      @test isapprox(psi1, psi2; atol=1E-8)
+    end
+
+    @testset "HilbertSpaceRepresentation" begin
+      hsr = represent(hs)
+      psi1 = ExactDiagonalization.Toolkit.product_state(hsr, local_states)
+      psi2 = zeros(ComplexF64, 16)
+      psi2[9] = 1.0 + 10.0im
+      psi2[11] = 2.0
+      @test isapprox(psi1, psi2; atol=1E-8)
+    end
+    @testset "sector-rep" begin
+      hssr = represent(HilbertSpaceSector(hs, 0))
+      # 0011, 0101, 0110, 1001, 1010, 1100
+      psi1 = ExactDiagonalization.Toolkit.product_state(hssr, local_states)
+      psi2 = zeros(ComplexF64, 6)
+      psi2[5] = 2.0
+      @test isapprox(psi1, psi2; atol=1E-8)
+    end
   end
 end
