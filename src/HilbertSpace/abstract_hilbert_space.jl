0270813bd2e74164b1b6eeb69c8126c850eb6681
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-02-03 17:43:38 -0500

    Export AbstractQuantumNumber

--- src/HilbertSpace/abstract_hilbert_space.jl
+++ src/HilbertSpace/abstract_hilbert_space.jl
@@ -2,6 +2,7 @@ export AbstractHilbertSpace
 abstract type AbstractHilbertSpace end
 
 ## TODO: Think about this
+export AbstractQuantumNumber
 AbstractQuantumNumber = Integer
 
 export AbstractSiteType
