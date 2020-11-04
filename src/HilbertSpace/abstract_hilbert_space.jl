91b0af683c46cb284969e43fc254cc0b290907db
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-04 09:48:04 -0500

    Remove AbstractSiteType (unused)

--- src/HilbertSpace/abstract_hilbert_space.jl
+++ src/HilbertSpace/abstract_hilbert_space.jl
@@ -1,11 +1,7 @@
 export AbstractQuantumNumber
 export AbstractHilbertSpace
-export AbstractSiteType
-
 
 ## TODO: Think about this
 AbstractQuantumNumber = Integer
 
 abstract type AbstractHilbertSpace{QN<:Tuple{Vararg{<:AbstractQuantumNumber}}} end
-
-abstract type AbstractSiteType end
