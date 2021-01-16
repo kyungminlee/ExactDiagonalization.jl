c51fea04be27a984a4f50406abd44ed70536de01
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-01-16 10:32:06 -0500

    Update documentation

--- docs/make.jl
+++ docs/make.jl
@@ -9,19 +9,22 @@ makedocs(
     authors="Kyungmin Lee",
     checkdocs=:all,
     pages = [
-      "Home" => "index.md",
-      "Types" => [
-        "Hilbert space" => "hilbertspace.md",
-        "Operator" => "operator.md",
-        "Representation" => "representation.md",
-        "Symmetry" => "symmetry.md"
-      ],
-      "Examples" => [
-          "examples/spinhalf.md",
-      ],
-      "Index" => "links.md",
-      "API" => "api.md",
+        "Home" => "index.md",
+        "Types" => [
+            "Hilbert space" => "hilbertspace.md",
+            "Operator" => "operator.md",
+            "Representation" => "representation.md",
+            "Symmetry" => "symmetry.md"
+        ],
+        "Examples" => [
+            "examples/spinhalf.md",
+        ],
+        "Index" => "links.md",
+        "API" => "api.md",
     ]
   )
 
-deploydocs(repo="github.com/kyungminlee/ExactDiagonalization.jl.git", devbranch = "dev")
+deploydocs(;
+    repo="github.com/kyungminlee/ExactDiagonalization.jl.git",
+    devbranch="dev",
+)
